927b74cf3c1650acace852eca38ae9389aa62fba
ParekhAmi <amiparekh87@gmail.com>
2021-01-16 02:18:26 -0500

    State-Manipulation and Handling event

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -1,23 +1,49 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person.js';
 
 class App extends Component {
+
+  state={
+    persons:[
+        {name:"Mukesh shah",age:40},
+        {name:"Mehul CHavada",age:30},
+        {name:"Ami Parekh",age:26}
+       ]
+  }
+
+  switchNameHandler =()=>{
+    console.log('BUtton is PRessed.');
+    this.setState({
+      persons:[
+        {name:"Ankit THakor",age:22},
+        {name:"Nirali Parmar",age:19},
+        {name:"Mitul",age:27}
+       ]
+    })
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-          <hr></hr>
-          <b>This is my first project app in React JS.</b>
+          <h1 className="App-title">React JS</h1>
           
-        </p>
-      </div>
+        </header>
+        
+         <h1 className="App-intro"> React JS App </h1>
+         <p>React App learning..let's get started. </p>
+         <button onClick={this.switchNameHandler}>Switch Names</button>
+         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobbies: Music</Person>
+         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+          
+      </div> 
+     
     );
+
+    //return React.createElement('div',{className:'App'},React.createElement('h1',null,'Hi,I\'m learning React'));
   }
 }
 
