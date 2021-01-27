4b7a51c4e6473f7db7332522ceb0618687600083
ParekhAmi <amiparekh87@gmail.com>
2021-01-27 08:18:31 -0500

    Flexible list-name changed using person index

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -28,19 +28,23 @@ class App extends Component {
     this.setState({persons: persons});
   }
   
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Ankit', age: 22 },
-        {
-          name: event.target.value, age: 19, hob:
-          {
-            priority: []
-          }
-        },
-        { name: "Mitul", age: 27 }
-      ]
+  nameChangedHandler = (event,id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
     });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    // const person = Object.assign({},this.state.persons[personIndex]);
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+  
+    this.setState({persons: persons});
   }
 
   togglePersonsHandler = () => {
@@ -67,7 +71,8 @@ class App extends Component {
               click={() => this.deletePersonHandler(index)}
               name={person.name}
               age={person.age} 
-              key={person.id}/>
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event,person.id)}/>
           })}
 
         </div>);
