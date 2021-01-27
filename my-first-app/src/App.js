9f051c371316941c98f42ebf1d07916a929f3598
ParekhAmi <amiparekh87@gmail.com>
2021-01-27 00:02:36 -0500

    Output list

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -71,18 +71,12 @@ class App extends Component {
               
                 person =(
                 <div>
-                <Person
-                  name={this.state.persons[0].name}
-                  age={this.state.persons[0].age} />
-                <Person
-                  name={this.state.persons[1].name}
-                  age={this.state.persons[1].age}
-                  click={this.switchNameHandler.bind(this, 'Krushna')}
-                  changed={this.nameChangedHandler}>
-                  Hobbies: {this.state.persons[1].hob.priority} </Person>
-                <Person
-                  name={this.state.persons[2].name}
-                  age={this.state.persons[2].age} />
+                  {this.state.persons.map(person => {
+                    return <Person 
+                    name = {person.name}
+                    age = {person.age}/>
+                  })}
+                
               </div> );
 
               }
