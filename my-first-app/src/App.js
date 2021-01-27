b2b25df41139f6a9de9e4bbd6fc9e2603b3c770d
ParekhAmi <amiparekh87@gmail.com>
2021-01-27 02:17:09 -0500

    Delete one element from array using index in map function

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -8,36 +8,26 @@ class App extends Component {
 
   state = {
     persons: [
-                { name:"Mukesh shah", age:40},
-                { name:"Mehul CHavada", age:30, hob:
-                  {
-                    priority: ['FINANCE , ', 'SHARE , ', 'MUTUAL FUND']
-                }},
-                { name: "Ami Parekh", age: 26 }
-          ],
-          otherState: 'some other value',
-          showPersons: false
-  }
-  
-
- switchNameHandler = (newName) => {
-    console.log('BUtton is PRessed.');
-    this.setState({
-      persons: [
-        { name: newName, age: 22 },
+      { name: "Mukesh shah", age: 40 },
+      {
+        name: "Mehul CHavada", age: 30, hob:
         {
-          name: "Nirali Parmar", age: 19, hob:
-          {
-            priority: []
-          }
-        },
-        { name: "Mitul", age: 27 }
-      ]
-    });
+          priority: ['FINANCE , ', 'SHARE , ', 'MUTUAL FUND']
+        }
+      },
+      { name: "Ami Parekh", age: 26 }
+    ],
+    otherState: 'some other value',
+    showPersons: false
+  }
+
+  deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons;
+    persons.splice(personIndex,1);
+    this.setState({persons: persons});
   }
   
-  
- nameChangedHandler = (event) => {
+  nameChangedHandler = (event) => {
     this.setState({
       persons: [
         { name: 'Ankit', age: 22 },
@@ -51,55 +41,56 @@ class App extends Component {
       ]
     });
   }
-  
+
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow });
+    this.setState({ showPersons: !doesShow });
   }
-  render(){
-              const style = {
-                backgroundColor: 'White',
-                border: '1px solid blue',
-                font: 'inherit',
-                padding: '8px',
-                cursor: 'pointer'
+  render() {
+    const style = {
+      backgroundColor: 'White',
+      border: '1px solid blue',
+      font: 'inherit',
+      padding: '8px',
+      cursor: 'pointer'
 
-              };
+    };
 
-              let person = null;
-              if (this.state.showPersons) {
-              
-                person =(
-                <div>
-                  {this.state.persons.map(person => {
-                    return <Person 
-                    name = {person.name}
-                    age = {person.age}/>
-                  })}
-                
-              </div> );
+    let person = null;
+    if (this.state.showPersons) {
 
-              }
+      person = (
+        <div>
+          {this.state.persons.map((person,index) => {
+            return <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age} />
+          })}
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">React JS</h1>
-      </header>
-      <h1 className="App-intro"> React JS App </h1>
-      <p>React App learning..let's get started. </p>
-       <button
-            style={style}
-             onClick={this.togglePersonsHandler}>Toggle Persons</button>
-             
-             {person}
-             
-            </div>
+        </div>);
 
-  );
+    }
 
-  //return React.createElement('div',{className:'App'},React.createElement('h1',null,'Hi,I\'m learning React'));
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">React JS</h1>
+        </header>
+        <h1 className="App-intro"> React JS App </h1>
+        <p>React App learning..let's get started. </p>
+        <button
+          style={style}
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+
+        {person}
+
+      </div>
+
+    );
+
+    //return React.createElement('div',{className:'App'},React.createElement('h1',null,'Hi,I\'m learning React'));
   }
 }
 
