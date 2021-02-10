26d0ea479fcbfd8c176b962329a69b6719b0e012
ParekhAmi <amiparekh87@gmail.com>
2021-02-10 02:10:57 -0500

    Transform ingredients object into array with key and value

--- burger-app/src/components/Burger/Burger.js
+++ burger-app/src/components/Burger/Burger.js
@@ -3,12 +3,25 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import '../Burger/Burger.css';
 
 const burger = (props) => {
+
+    let transformIngredient = Object.keys(props.ingredients) 
+                .map(IgKey=>{
+                    return [...Array(props.ingredients[IgKey])].map((_,i)=>{
+                      return   <BurgerIngredient key={IgKey+i} type={IgKey} />
+                    });
+                }).reduce((arr,el)=>{
+                    return arr.concat(el);
+        },[]);
+
+    if(transformIngredient.length===0)
+    {
+        transformIngredient=<p>Please adding an Ingrediant to make Burger tasteful.</p>;
+    }
+    console.log(transformIngredient);
     return(
         <div class="Burger">
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
-            <BurgerIngredient type="salad"/>
+            {transformIngredient}
             <BurgerIngredient type="bread-bottom"/>
 
         </div>
