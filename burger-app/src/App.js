863c26b987ac634a5270076f31a55530afb86763
ParekhAmi <amiparekh87@gmail.com>
2021-02-10 02:16:25 -0500

    nothing changed

--- burger-app/src/App.js
+++ burger-app/src/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
-import BurgerBuilder from './containers/BurgerBuilder';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+
 
 class App extends Component {
   render()
    {
       return (
-          <div>
-            <Layout >
-                <p>Hello Bond</p>
-                <BurgerBuilder />          
+            <Layout>   
+              <BurgerBuilder />
             </Layout>    
-          </div>
         );
       }
 }
