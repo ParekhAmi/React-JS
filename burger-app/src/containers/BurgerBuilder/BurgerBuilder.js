277e2d8d0510eb1240675522816bc4bb41419ce4
ParekhAmi <amiparekh87@gmail.com>
2021-02-10 02:07:13 -0500

    Add ingredients in state as an object

--- burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,19 @@ import Aux from '../../hoc/Aux1';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+
+    state={
+        ingredients:{
+            salad:0,
+            cheese:0,
+            meat:0,
+            bacon:0
+         }
+    }
     render() {
         return (
             <Aux>
-                <Burger />
+                <Burger ingredient={this.state.ingredients} />
                 <div>Build controls</div>                
             </Aux>
         );   
