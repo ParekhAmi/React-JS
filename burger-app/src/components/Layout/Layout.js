3e1fccb95da428d3045a9ada14bfa69c91828b4f
ParekhAmi <amiparekh87@gmail.com>
2021-02-10 02:14:33 -0500

    import Layout.css in Layout.js file

--- burger-app/src/components/Layout/Layout.js
+++ burger-app/src/components/Layout/Layout.js
@@ -1,11 +1,11 @@
-
 import React from 'react';
 import Aux from '../../hoc/Aux1';
-import classes from './Layout.css';
+import './Layout.css';
+
 const layout = (props)=>(    
     <Aux>
         <div >Toolbar, Side Bar, Drawer</div>
-        <main class="Container" >
+        <main class="Container">
             {props.children} 
         </main>
     </Aux>
