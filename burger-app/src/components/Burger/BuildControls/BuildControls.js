1ea07f1ac2b5a17d8a6238beb4cd36bd3fd80199
ParekhAmi <amiparekh87@gmail.com>
2021-02-11 07:16:10 -0500

    Dynamically Add and remove of ingredients...And price of each ingredients

--- burger-app/src/components/Burger/BuildControls/BuildControls.js
+++ burger-app/src/components/Burger/BuildControls/BuildControls.js
@@ -3,19 +3,32 @@ import './BuildControls.css';
 import BuildControl from './BuildControl/BuildControl';
 
 const controls = [
-    {label: 'Salad', type:'salad'},
-    {label: 'Cheese', type:'cheese'},
-    {label: 'Meat', type:'meat'},
-    {label: 'Bacon', type:'bacon'}
+    { label: 'Salad', type: 'salad' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' },
+    { label: 'Bacon', type: 'bacon' }
 ];
 
-const buildControls =(props) => (
+const buildControls = (props) => (
 
     <div class="BuildControls">
-            
+        <p>Current Price : {props.price}</p>
+
         {controls.map(ctrl => (
-            <BuildControl key={ctrl.label} label={ctrl.label} />    
+            <BuildControl
+                key={ctrl.label}
+                label={ctrl.label}
+                disabled={props.disabled[ctrl.type]}
+                iprice={props.iprice[ctrl.type]}
+                added={() => props.ingredientAdded(ctrl.type)}
+                removed={() => props.ingredientRemoved(ctrl.type)}
+                newItemCount={props.newItemCount[ctrl.type]}
+
+            />
         ))}
+
+        <p>totalAllItemPrice: {props.itemPrice}</p>
+
     </div>
 );
 
