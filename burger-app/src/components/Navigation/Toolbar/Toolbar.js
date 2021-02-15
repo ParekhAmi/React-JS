409c339ad2a65cc0687771973d33af16ba3de1f2
ParekhAmi <amiparekh87@gmail.com>
2021-02-15 02:39:35 -0500

    Toolbar Component created

---
+++ burger-app/src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Toolbar.css';
+import Logo from '../Logo/Logo';
+
+const toolbar = (props) => (
+    <header class="Toolbar">
+        <div>MENU</div>
+        <Logo />
+        <nav>
+            ...
+        </nav>
+    </header>
+);
+
+export default toolbar;
