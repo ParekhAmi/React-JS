3da58c1290af63e57bacc6704f9d1729863de894
ParekhAmi <amiparekh87@gmail.com>
2021-02-15 02:40:49 -0500

    import Logo component

--- burger-app/src/components/Navigation/Toolbar/Toolbar.js
+++ burger-app/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Toolbar.css';
-import Logo from '../Logo/Logo';
+import Logo from '../../Logo/Logo';
 
 const toolbar = (props) => (
     <header class="Toolbar">
