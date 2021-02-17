a180db514df2d39c45c38affb5761d7c0612ad6c
ParekhAmi <amiparekh87@gmail.com>
2021-02-17 00:09:03 -0500

    import NavigationItems

--- burger-app/src/components/Navigation/Toolbar/Toolbar.js
+++ burger-app/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Toolbar.css';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+
 
 const toolbar = (props) => (
     <header class="Toolbar">
         <div>MENU</div>
         <Logo />
         <nav>
-            ...
+            <NavigationItems />
         </nav>
     </header>
 );
