451c002e4b9684f6e1b20b916aa7ecaa86478dc0
ParekhAmi <amiparekh87@gmail.com>
2021-02-19 03:09:41 -0500

    Add sideDrawerToggle Button

---
+++ burger-app/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const drawerToggle = (props) => (
+    <div class="DrawerToggle" onClick={props.clicked}>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+);
+
+export default drawerToggle;
