0c65f2050d4c6cb559d988ba122d08174c0ebbc6
ParekhAmi <amiparekh87@gmail.com>
2021-02-19 02:26:17 -0500

    Button class passed as Name property

--- burger-app/src/components/UI/Button/Button.js
+++ burger-app/src/components/UI/Button/Button.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import './Button.css';  
-
 
 const button = (props) => (
     <button 
     onClick={props.clicked}
-    class="Button">{props.children}</button>
+    class={props.Name}>{props.children}</button>
 );
 
 export default button;
