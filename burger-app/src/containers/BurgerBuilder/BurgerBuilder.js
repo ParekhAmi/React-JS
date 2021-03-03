d38beddfbe97162f3d30fd761f5bc9016d723f72
ParekhAmi <amiparekh87@gmail.com>
2021-03-03 00:13:05 -0500

    Add query params into URL using join &

--- burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,7 +4,8 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
-import axios from '../../components/Layout/axios-orders';
+//import axios from '../../components/Layout/axios-orders';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -26,7 +27,9 @@ class BurgerBuilder extends Component {
         itemCount: 0,
         itemPrice: 0,
         purchasable: false,
-        purchasing: false
+        purchasing: false,
+        loading: false
+        
     }
 
     updatePurchaseState(ingredients) {
@@ -92,25 +95,46 @@ class BurgerBuilder extends Component {
         this.setState({purchasing: false});
     }
     purchaseContinueHandler = () => {
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Ami Parekh',
-                address: {
-                    street: 'Iscon 1',
-                    zipcode: '388120',
-                    country: 'India'
-                },
-                email: 'ami87@gmail.com'
-            },
-            deliveryMethod: 'fastest'
-        }
-        axios.post('/orders.json',order)
-        .then(response => console.log(response))
-        .catch(error => console.log(error));
-    }
+        // let newDate = new Date()
+        // let date = newDate.getDate();
+        // let month = newDate.getMonth() + 1;
+        // let year = newDate.getFullYear();
 
+        // let ODate = date+"/"+month+"/"+year;
+        // this.setState({loading: true});
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Ami Parekh',
+        //         address: {
+        //             street: 'Iscon 1',
+        //             zipcode: '388120',
+        //             country: 'India'
+        //         },
+        //         email: 'ami87@gmail.com'
+        //     },
+        //     deliveryMethod: 'fastest',
+        //     orderDate :  ODate
+        // }
+        // axios.post('/orders.json',order)
+        // .then(response => {
+        //     this.setState({loading: false,purchasing: false});
+        // })
+        // .catch(error => {
+        //     this.setState({loading: false,purchasing: false});
+        // });
+        const queryParams=[];
+        for (let i in this.state.ingredients){
+            queryParams.push(encodeURIComponent(i)+ "=" + encodeURIComponent(this.state.ingredients[i]));
+        }
+        const queryString = queryParams.join('&');
+
+        this.props.history.push({
+                pathname:'/checkout',
+                search : '?'+queryString
+        });
+    }
     render() {
 
         const disableInfo = {
@@ -120,16 +144,22 @@ class BurgerBuilder extends Component {
         for (let key in disableInfo) {
             disableInfo[key] = disableInfo[key] <= 0
         }
+
+        let orderSummary = <OrderSummary 
+            ingredients={this.state.ingredients}
+            purchaseCanceled={this.purchaseCancel}
+            purchaseContinued={this.purchaseContinueHandler}
+            price={this.state.totalPrice}/>;
+
+            if (this.state.loading) {
+                orderSummary = <Spinner />;
+            }
         return (
 
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
                 <Modal show={this.state.purchasing} modelClosed={this.purchaseCancel }>
-                    <OrderSummary 
-                    ingredients={this.state.ingredients}
-                    purchaseCanceled={this.purchaseCancel}
-                    purchaseContinued={this.purchaseContinueHandler}
-                    price={this.state.totalPrice}/>
+                    {orderSummary}
                 </Modal>
                 <BuildControls
                     ingredientAdded={this.addIngredientHandler}
