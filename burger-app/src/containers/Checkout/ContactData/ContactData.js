387ca372eb6d8f53594c2c8e31486528fa2a5562
ParekhAmi <amiparekh87@gmail.com>
2021-03-08 01:34:04 -0500

    Setting up JS config for form

--- burger-app/src/containers/Checkout/ContactData/ContactData.js
+++ burger-app/src/containers/Checkout/ContactData/ContactData.js
@@ -1,71 +1,108 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './ContactData.css';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import Button from '../../../components/UI/Button/Button';
 import axios from '../../../components/Layout/axios-orders';
+import Input from '../../../components/UI/Input/Input';
 
 
 class ContactData extends Component {
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalcode: ''
+        orderForm: {
+           
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
+                },
+                value: ''
+            },  
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street'
+                },
+                value: ''
+            },
+            zipcode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'ZIP Code'
+                },
+                value: ''
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Country'
+                },
+                value: ''
+            },    
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your E-Mail'
+                },
+                value: ''
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                options: [
+                    {value: 'fastest', displayValue: 'Fastest'},
+                    {value: 'cheapest', displayValue: 'Cheapest'}
+                ],
+                value: ''
+            }
         },
+
         loading: false
     }
 
     orderHandler = (event) => {
         event.preventDefault();
-          let newDate = new Date()
+        let newDate = new Date()
         let date = newDate.getDate();
         let month = newDate.getMonth() + 1;
         let year = newDate.getFullYear();
 
-        let ODate = date+"/"+month+"/"+year;
-        this.setState({loading: true});
+        let ODate = date + "/" + month + "/" + year;
+        this.setState({ loading: true });
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,
-            customer: {
-                name: 'Ami Parekh',
-                address: {
-                    street: 'Iscon 1',
-                    zipcode: '388120',
-                    country: 'India'
-                },
-                email: 'ami87@gmail.com'
-            },
-            deliveryMethod: 'fastest',
-            orderDate :  ODate
+            orderDate: ODate
         }
-        axios.post('/orders.json',order)
-        .then(response => {
-            this.props.history.push("/");
-            this.setState({loading: false});
-        })
-        .catch(error => {
-            this.setState({loading: false});
-        });
+        axios.post('/orders.json', order)
+            .then(response => {
+                this.props.history.push("/");
+                this.setState({ loading: false });
+            })
+            .catch(error => {
+                this.setState({ loading: false });
+            });
     }
     render() {
-        let form = ( 
-        <form>
-            <input class="Input" type="text" name="name" placeholder="Your Name" />
-            <input class="Input" type="email" name="email" placeholder="Your Email" />
-            <input class="Input" type="text" name="street" placeholder="Street" />
-            <input class="Input" type="text" name="postalcode" placeholder="PostalCode" />
-            <Button Name="Button Success" clicked={this.orderHandler}>Order</Button>
-        </form>
+        let form = (
+            <form>
+                <Input elementType="..." elementConfig="..." value="..." />
+                <Input inputtype="input" type="email" name="email" placeholder="Your Email" />
+                <Input inputtype="input" type="text" name="street" placeholder="Street" />
+                <Input inputtype="input" type="text" name="postalcode" placeholder="PostalCode" />
+                <Button Name="Button Success" clicked={this.orderHandler}>Order</Button>
+            </form>
         );
-        if(this.state.loading) {
+        if (this.state.loading) {
             form = <Spinner />
         }
-        return(
+        return (
             <div class="ContactData">
                 <h4>Enter Contact Data</h4>
-               {form}
+                {form}
             </div>
         );
     }
