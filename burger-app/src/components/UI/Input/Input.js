911074107b5c3d378e9031252ef8c332e35d28a8
ParekhAmi <amiparekh87@gmail.com>
2021-03-09 03:31:25 -0500

    Added select as an elementType

--- burger-app/src/components/UI/Input/Input.js
+++ burger-app/src/components/UI/Input/Input.js
@@ -5,49 +5,48 @@ const Input = (props) => {
 
     let inputElement = null;
 
-    switch(props.elementType)
-    {
-            case ('input') : 
-                inputElement= <input 
-                class="InputElement" 
-                {...props.elementConfig} 
-                value={props.value}/>
-                break;
-            case ('textarea'):
-                inputElement= <textarea 
-                class="InputElement" 
-                {...props.elementConfig} 
-                value={props.value}/>
-                break;
-            case ('select'):
-                inputElement= (
-                <select 
+    switch (props.elementType) {
+        case ('input'):
+            inputElement = <input
+                class="InputElement"
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed} />
+            break;
+        case ('textarea'):
+            inputElement = <textarea
+                class="InputElement"
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed} />
+            break;
+        case ('select'):
+            inputElement = (
+                <select
                     class="InputElement"
-                    {...props.elementConfig} 
-                    value={props.value}>
+                    value={props.value}
+                    onChange={props.changed}>
                     {props.elementConfig.options.map(option => (
                         <option key={option.value} value={option.value}>
                             {option.displayValue}
                         </option>
-                    ))}    
-                    </select>
-                );
-                break;
-            default :
-                inputElement= <input 
-                class="InputElement" 
-                {...props.elementConfig} 
-                value={props.value}/>
+                    ))}
+                </select>
+            );
+            break;
+        default:
+            inputElement = <input
+                class="InputElement"
+                {...props.elementConfig}
+                value={props.value} />
     }
 
-    return(
-                <div class="Input">
-                    <label class="Label">{props.label}</label>
-                    {inputElement}
-                </div>
-
-        );
-
+    return (
+        <div class="Input">
+            <label class="Label">{props.label}</label>
+            {inputElement}
+        </div>
+    );
 };
 
 export default Input;
