d91b1bebc9009d5c3386a4aae663e5ac6c09f71c
ParekhAmi <amiparekh87@gmail.com>
2021-03-12 01:37:48 -0500

    Just format file

--- burger-app/src/components/Burger/Burger.js
+++ burger-app/src/components/Burger/Burger.js
@@ -1,31 +1,31 @@
 import React from 'react';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import '../Burger/Burger.css';
+import { withRouter } from 'react-router-dom';
 
 const burger = (props) => {
+    console.log(props);
+    let transformIngredient = Object.keys(props.ingredients)
+        .map(IgKey => {
+            return [...Array(props.ingredients[IgKey])].map((_, i) => {
+                return <BurgerIngredient key={IgKey + i} type={IgKey} />
+            });
+        }).reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
 
-    let transformIngredient = Object.keys(props.ingredients) 
-                .map(IgKey=>{
-                    return [...Array(props.ingredients[IgKey])].map((_,i)=>{
-                      return <BurgerIngredient key={IgKey+i} type={IgKey}/>
-                    });
-                }).reduce((arr,el)=>{
-                    return arr.concat(el);
-        },[]);
-
-    if(transformIngredient.length===0)
-    {
-        transformIngredient=<p>Please adding an Ingrediant to make Burger tasteful.</p>;
+    if (transformIngredient.length === 0) {
+        transformIngredient = <p>Please adding an Ingrediant to make Burger tasteful.</p>;
     }
-    
-    return(
+
+    return (
         <div class="Burger">
-            <BurgerIngredient type="bread-top"/>
+            <BurgerIngredient type="bread-top" />
             {transformIngredient}
-            <BurgerIngredient type="bread-bottom"/>
+            <BurgerIngredient type="bread-bottom" />
 
         </div>
     );
 };
 
-export default burger;
+export default withRouter(burger);
