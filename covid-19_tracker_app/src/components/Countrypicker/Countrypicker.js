ebcbf13b4c4ee3b65a5ebced26613307bf532572
ParekhAmi <amiparekh87@gmail.com>
2021-07-30 01:14:52 +0530

    added spread operator in setstate

--- covid-19_tracker_app/src/components/Countrypicker/Countrypicker.js
+++ covid-19_tracker_app/src/components/Countrypicker/Countrypicker.js
@@ -1,40 +1,41 @@
 import React, { useState } from 'react';
 import './Countrypicker.css';
-import axios from 'axios';
+import axios from '../Axios/Axios';
 
 
-const Countrypicker = () => {
+const Countrypicker = (props) => {
 
     const initialState = [{ name: '---Select country---', code: '---' }];
     const [listCountries, setListCountries] = useState(initialState);
 
 
-    axios.get('https://covid.mathdro.id/api/countries')
+    axios.get('/countries')
         .then(res => {
             //console.log(res.data.countries);
-            setListCountries(res.data.countries);
+            setListCountries(listCountries => [...listCountries, res.data.countries]);
         })
         .catch(error => {
             console.log(error);
         })
 
-    console.log(listCountries);
+    //console.log(listCountries);
 
     return (
 
-        <div class='Countrypicker'>
+        <div className='Countrypicker'>
 
             {
                 <select>
                     {
                         listCountries.map(country => {
                             return (
-                            <option value={country.iso3}>{country.name}</option>
-                        )
+                                <option value={country.iso3}>{country.name}</option>
+                            )
                         })
                     }
                 </select>
             }
+            {props.children}
         </div>
 
     );
