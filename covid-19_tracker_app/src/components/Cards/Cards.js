db654fda29852d40126892920978090d5bc66697
ParekhAmi <amiparekh87@gmail.com>
2021-07-30 01:11:34 +0530

    Created different blocks for Confirmed,Recovered,Deaths

--- covid-19_tracker_app/src/components/Cards/Cards.js
+++ covid-19_tracker_app/src/components/Cards/Cards.js
@@ -1,18 +1,69 @@
 import React from 'react';
-import instance from '../Axios/Axios';
-import 'Cards.css';
+import axios from '../Axios/Axios';
+import './Cards.css';
 
 const Cards = () => {
-    const basicCounter = instance.get('/confirmed')
+
+
+    // let one = "https://covid19.mathdro.id/api/confirmed";
+    // let two = "https://covid19.mathdro.id/api/recovered";
+    // let three = "https://covid19.mathdro.id/api/deaths";
+
+
+    // const requestOne = axios.get(one);
+    // const requestTwo = axios.get(two);
+    // const requestThree = axios.get(three);
+
+    // axios.all([requestOne,requestTwo,requestThree])
+    //     .then(axios.spread((...responses) => {
+    //         const responseOne = responses[0]
+    //         const responseTwo = responses[1]
+    //         const responseThree = responses[2]
+
+    //         console.log(responseOne,responseTwo,responseThree);
+        
+    //     }))
+    //     .catch(error => {
+    //         console.log(error);
+    //     })
+
+        
+        
+
+    axios.get('/confirmed')  
             .then(res => {
-                console.log(res);
+                console.log(res.data);
             })
             .catch(error => {
                 console.log(error);
             })
+
+            axios.get('/recovered')
+                    .then(res => {
+                        console.log(res.data);
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    })
+
+                    axios.get('/deaths')  
+                            .then(res => {
+                                console.log(res.data);
+                            })
+                            .catch(error => {
+                                console.log(error);
+                            })    
     return (
         <div class="Cards">
-            Confirmed {basicCounter}
+            <div class="Confirmed">
+
+            </div>
+            <div class="Recovered">
+
+            </div>
+            <div class="Deaths">
+                
+            </div>
         </div>
     );
 }
